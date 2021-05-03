e35a514a4825eb6c88b06feb8ddf5c678c43b16b
datsniki <81483328+datsniki@users.noreply.github.com>
2021-05-03 12:03:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+require_once "controllers/c_conexion.php";
+
+require_once "models/m_header.php";
+require_once "models/m_index.php";
+require_once "models/m_entrPred.php";
+require_once "models/m_footer.php";
