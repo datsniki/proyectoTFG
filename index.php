5f4dd41d21c7501baf84e047d6011fc3d04f40bb
datsniki <81483328+datsniki@users.noreply.github.com>
2021-05-19 16:27:04 +0200

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once "controllers/c_conexion.php";
-
-require_once "models/m_header.php";
-require_once "models/m_index.php";
-require_once "models/m_entrPred.php";
-require_once "models/m_footer.php";
